85dd54146d4331d97cb24b088a28a4a4b07bd712
JoaoZ14 <csgojoao84@gmail.com>
2026-04-16 18:53:37 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Entrada alternativa na raiz (lista de deteção Vercel inclui index.js).
+ */
+export { default } from './server.js';
---
+++ src/server.js
@@ -0,0 +1,7 @@
+/**
+ * Entrada Vercel quando Root Directory = `src` (path0 = pasta src).
+ * @see https://vercel.com/docs/frameworks/backend/express
+ */
+import { createApp } from './expressApp.js';
+
+export default createApp();
